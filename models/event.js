b7dc8a644490f39d1bbc7f0dd8875c0a9aab9f8f
farah-battikh <farah.battikh@esprit.tn>
2024-04-05 07:00:00 +0100

    mongoo

--- models/event.js
+++ models/event.js
@@ -7,7 +7,7 @@ const eventSchema = new mongoose.Schema({
   organisateur: String,
   dateDebut: Date,        // Ajout de la date de début (utilisation du type Date)
   dateFin: Date,          // Ajout de la date de fin (utilisation du type Date)
-  participants: [String], // Ajout de la liste des participants (tableau de chaînes)
+  participants: [String], // Ajout de  liste des participants (tableau de chaînes)
 });
 
 module.exports = mongoose.model('Event', eventSchema);
